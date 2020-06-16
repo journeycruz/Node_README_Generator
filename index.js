004f84c0ebe320c83350fcf78419f235fdd9b208
Journey Cruz <journey.cruz@gmail.com>
2020-06-16 14:05:00 -0500

    add writefileasync function and add questions for inquirer to prompt

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inquirer = require('inquirer');
 const util = require('util');
 const axios = require('axios')
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 function promptUser() {
     return inquirer.prompt(
         {
@@ -10,8 +12,57 @@ function promptUser() {
             name: "username"
         },
         {
-            type: "input"
-        })
+            type: "input",
+            message: "What is the title of this project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter a description",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Enter a table of contents",
+            name: "tableOfContents"
+        },
+        {
+            type: "input",
+            message: "Does this project require installation of any third party tools? (i.e. inquirer, axios, etc...)",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "Select a license",
+            choices: [
+                "MIT", 
+                "GNU GPLv3", 
+                "Mozilla Public License 2.0", 
+                "ApacheLicense2.0"
+              ],
+              name: "license"
+        },
+        {
+            type: "input",
+            message: "Enter guidelines for how other developers can edit this project",
+            name: "contributing"
+        },
+        {
+            type: "input",
+            message: "Are there any tests that you would recommend for this project?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "Are there any questions with this project?",
+            name: "questions"
+        }
+        )
         .then(function ({ username }) {
             const queryUrl = `https://api.github.com/users/${username}`;
 
@@ -25,5 +76,7 @@ function promptUser() {
         })
 }
 
-
 inqPromise = promptUser()
+inqPromise.then(function(userInput) {
+    let readMe = ``
+})
