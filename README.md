6adfcb13dbee5cd0cddee43f49bc640817cdd997
Journey Cruz <journey.cruz@gmail.com>
2020-06-16 16:06:46 -0500

    remove util and writefileasync remove promptuser() use throw instead of catch at end

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Node_README_Generator
+# title
+
+            ## Description 
+            
+            description    
+            
+            
+            ## Table of Contents (Optional)
+            
+            change this next
+            
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [Credits](#credits)
+            * [License](#license)
+            
+            
+            ## Installation
+            
+            inquirer and axios
+            
+            
+            ## Usage 
+            
+            example 
+            
+            
+            ## License
+            
+            ApacheLicense2.0
+        
+            ## Badges
+            
+            
+            ## Contributing
+            
+            free to use
+            
+            ## Tests
+            
+            test
+            
+            null
+
+            https://avatars2.githubusercontent.com/u/62083284?v=4
+            
--- index.js
+++ index.js
@@ -1,130 +1,116 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
 const axios = require('axios')
 
-const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-    return inquirer.prompt(
-        {
-            message: "Enter your GitHub username:",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "What is the title of this project?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Enter a description",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "Enter a table of contents",
-            name: "tableOfContents"
-        },
-        {
-            type: "input",
-            message: "Does this project require installation of any third party tools? (i.e. inquirer, axios, etc...)",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "Provide instructions and examples for use",
-            name: "usage"
-        },
-        {
-            type: "list",
-            message: "Select a license",
-            choices: [
-                "MIT", 
-                "GNU GPLv3", 
-                "Mozilla Public License 2.0", 
-                "ApacheLicense2.0"
-              ],
-              name: "license"
-        },
-        {
-            type: "input",
-            message: "Enter guidelines for how other developers can edit this project",
-            name: "contributing"
-        },
-        {
-            type: "input",
-            message: "Are there any tests that you would recommend for this project?",
-            name: "tests"
-        },
-        {
-            type: "input",
-            message: "Are there any questions with this project?",
-            name: "questions"
-        }
-        )
-        .then(function ({ username }) {
-            const queryUrl = `https://api.github.com/users/${username}`;
 
-            axios.get(queryUrl).then(function (res) {
-                const userEmail = res.data.email;
-                const userAvi = res.data.avatar_url;
-            })
-        })
-}
+inquirer
+    .prompt([{
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "username"
+    }, {
+        type: "input",
+        message: "What is the title of this project?",
+        name: "title"
+    }, {
+        type: "input",
+        message: "Enter a description",
+        name: "description"
+    }, {
+        type: "input",
+        message: "Enter a table of contents",
+        name: "tableOfContents"
+    }, {
+        type: "input",
+        message: "Does this project require installation of any third party tools? (i.e. inquirer, axios, etc...)",
+        name: "installation"
+    }, {
+        type: "input",
+        message: "Provide instructions and examples for use",
+        name: "usage"
+    }, {
+        type: "list",
+        message: "Select a license",
+        choices: [
+            "MIT",
+            "GNU GPLv3",
+            "Mozilla Public License 2.0",
+            "ApacheLicense2.0"
+        ],
+        name: "license"
+    }, {
+        type: "input",
+        message: "Enter guidelines for how other developers can edit this project",
+        name: "contributing"
+    }, {
+        type: "input",
+        message: "Are there any tests that you would recommend for this project?",
+        name: "tests"
+    }, {
+        type: "input",
+        message: "Are there any questions with this project?",
+        name: "questions"
+    }])
+    .then((answers) => {
+        const queryUrl = `https://api.github.com/users/${answers.username}`;
 
-inqPromise = promptUser()
-inqPromise.then(function(userInput) {
-    let readMe = `# ${userInput.title}
+        axios.get(queryUrl).then(function (res) {
+            const userEmail = res.data.email;
+            const userAvi = res.data.avatar_url;
 
-    ## Description 
-    
-    ${userInput.description}    
-    
-    
-    ## Table of Contents (Optional)
-    
-    ${userInput.tableOfContents}
-    
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
-    
-    
-    ## Installation
-    
-    ${userInput.installation}
-    
-    
-    ## Usage 
-    
-    ${userInput.usage} 
-    
-    
-    ## License
-    
-    ${userInput.license}
+            let readMe = `# ${answers.title}
 
-    ## Badges
-    
-    
-    ## Contributing
-    
-    ${userInput.contributing}
-    
-    ## Tests
-    
-    ${userInput.tests}
-    `;
-    let writePromise = writeFileAsync("README.md", readMe, "utf8");
-    writePromise.then(function() {
-        console.log("Successfully wrote out to README.md!");
-    }).catch(function(err) {
-        console.log("Problem with writing file README.md");
-        console.log(err);
-    }).catch(function(err) {
-        console.log("Problem with inquirer prompt");
+            ## Description 
+            
+            ${answers.description}    
+            
+            
+            ## Table of Contents (Optional)
+            
+            ${answers.tableOfContents}
+            
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [Credits](#credits)
+            * [License](#license)
+            
+            
+            ## Installation
+            
+            ${answers.installation}
+            
+            
+            ## Usage 
+            
+            ${answers.usage} 
+            
+            
+            ## License
+            
+            ${answers.license}
+        
+            ## Badges
+            
+            
+            ## Contributing
+            
+            ${answers.contributing}
+            
+            ## Tests
+            
+            ${answers.tests}
+            
+            ${userEmail}
+
+            ${userAvi}
+            `;
+            fs.writeFile("README.md", readMe, (err) => {
+                if (err) throw err;
+                console.log("Successfully wrote to README.md!");
+            });
+        });
+    })
+    .catch(function (err) {
         console.log(err);
     });
-})
