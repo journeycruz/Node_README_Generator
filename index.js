ae7848921a1dc666eb7520ac24f6a0a610c27d13
Journey Cruz <journey.cruz@gmail.com>
2020-06-16 13:31:16 -0500

    return email and avi from api to variables that I can use to append to readme later

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const util = require('util');
 const axios = require('axios')
 
 function promptUser() {
-    return inquirer.prompt({
-        message: "Enter your GitHub username:",
-        name: "username"
-    })
-    .then(function({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
+    return inquirer.prompt(
+        {
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            type: "input"
+        })
+        .then(function ({ username }) {
+            const queryUrl = `https://api.github.com/users/${username}`;
 
-        axios.get(queryUrl).then(function(res) {
-            const userEmail = res.data.map(function(email) {
-                return email;
+            axios.get(queryUrl).then(function (res) {
+                console.log(res);
+                console.log(res.data.avatar_url);
+                console.log(res.data.email);
+                const userEmail = res.data.email;
+                const userAvi = res.data.avatar_url;
             })
         })
-    })
 }
+
 
 inqPromise = promptUser()
